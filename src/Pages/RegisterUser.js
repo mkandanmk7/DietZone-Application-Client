760a54b8dfc48c0d489d1d9502f15c751c36916f
muthu <mkandanmk7@gmail.com>
2021-11-26 12:22:32 +0530

    align

--- src/Pages/RegisterUser.js
+++ src/Pages/RegisterUser.js
@@ -56,107 +56,112 @@ export const RegisterUser = () => {
         ) : (
           data.error && <p className="signup-info">{data.error}</p>
         )}
-      </div>
-      <Formik
-        initialValues={{
-          name: "",
-          email: "",
-          password: "",
-        }}
-        validate={(values) => {
-          const errors = {};
-          if (values.name.length === 0) return (errors.name = "*Required");
-          else if (values.name.length < 3)
-            errors.name = "*Name should contain 3 Characters";
-          if (!validateEmail.test(values.email))
-            errors.email = "*Email is Invalid";
-          else if (values.email.length === 0) errors.email = "*Required";
-          if (values.password.length < 6) {
-            errors.password = "*Password must be 6 characters";
-          } else if (values.password.length > 12) {
-            errors.password = "*Password must be within 12 characters";
-          }
-          return errors;
-        }}
-        onSubmit={(values) => {
-          registerUser(values);
-        }}
-      >
-        {() => {
-          return (
-            <div className="form-parent">
-              {load ? (
-                <DotLoader size={70} />
-              ) : (
-                <div className="form-div">
-                  <h2 className="Sign Up">Sign Up</h2>
-                  <Form>
-                    <div className="formflex">
-                      <FaUser className="icon" />
-                      <Field
-                        type="text"
+
+        <Formik
+          initialValues={{
+            name: "",
+            email: "",
+            password: "",
+          }}
+          validate={(values) => {
+            const errors = {};
+            if (values.name.length === 0) return (errors.name = "*Required");
+            else if (values.name.length < 3)
+              errors.name = "*Name should contain 3 Characters";
+            if (!validateEmail.test(values.email))
+              errors.email = "*Email is Invalid";
+            else if (values.email.length === 0) errors.email = "*Required";
+            if (values.password.length < 6) {
+              errors.password = "*Password must be 6 characters";
+            } else if (values.password.length > 12) {
+              errors.password = "*Password must be within 12 characters";
+            }
+            return errors;
+          }}
+          onSubmit={(values) => {
+            registerUser(values);
+          }}
+        >
+          {() => {
+            return (
+              <div className="form-parent">
+                {load ? (
+                  <DotLoader size={70} />
+                ) : (
+                  <div className="form-div">
+                    <h2 className="Sign Up">Sign Up</h2>
+                    <Form>
+                      <div className="formflex">
+                        <FaUser className="icon" />
+                        <Field
+                          type="text"
+                          name="name"
+                          className="form-control"
+                          placeholder="FullName"
+                        />
+                      </div>
+                      <ErrorMessage
                         name="name"
-                        className="form-control"
-                        placeholder="FullName"
+                        className="text-dark"
+                        component="div"
                       />
-                    </div>
-                    <ErrorMessage
-                      name="name"
-                      className="text-dark"
-                      component="div"
-                    />
-                    <div className="formflex">
-                      <MdEmail className="icon" />
-                      <Field
-                        type="email"
+                      <div className="formflex">
+                        <MdEmail className="icon" />
+                        <Field
+                          type="email"
+                          name="email"
+                          className="form-control"
+                          placeholder="Email"
+                        />
+                      </div>
+                      <ErrorMessage
                         name="email"
-                        className="form-control"
-                        placeholder="Email"
+                        className="text-dark"
+                        component="div"
                       />
-                    </div>
-                    <ErrorMessage
-                      name="email"
-                      className="text-dark"
-                      component="div"
-                    />
-                    <div className="formflex">
-                      <FaLock className="icon" />
-                      <Field
-                        type={showpassword ? "password" : "text"}
+                      <div className="formflex">
+                        <FaLock className="icon" />
+                        <Field
+                          type={showpassword ? "password" : "text"}
+                          name="password"
+                          className="form-control"
+                          placeholder="Password"
+                        />
+                        <button
+                          type="button"
+                          className="passbtn"
+                          onClick={() => {
+                            Setshowpassward(!showpassword);
+                          }}
+                        >
+                          {showpassword ? (
+                            <AiFillEyeInvisible />
+                          ) : (
+                            <AiFillEye />
+                          )}
+                        </button>
+                      </div>
+                      <ErrorMessage
                         name="password"
-                        className="form-control"
-                        placeholder="Password"
+                        className="text-dark"
+                        component="div"
                       />
-                      <button
-                        type="button"
-                        className="passbtn"
-                        onClick={() => {
-                          Setshowpassward(!showpassword);
-                        }}
-                      >
-                        {showpassword ? <AiFillEyeInvisible /> : <AiFillEye />}
-                      </button>
-                    </div>
-                    <ErrorMessage
-                      name="password"
-                      className="text-dark"
-                      component="div"
-                    />
-                    <div className="text-center mt-3">
-                      <button className="btn btn-outline-light" type="submit">
-                        Sign Up
-                      </button>
-                    </div>
-                  </Form>
-                  <Link to="/" className="link">
-                    Login ?
-                  </Link>
-                </div>
-              )}
-            </div>
-          );
-        }}
-      </Formik>
+                      <div className="text-center mt-3">
+                        <button className="btn btn-outline-light" type="submit">
+                          Sign Up
+                        </button>
+                      </div>
+                    </Form>
+                    <Link to="/" className="link">
+                      Login ?
+                    </Link>
+                  </div>
+                )}
+              </div>
+            );
+          }}
+        </Formik>
+      </div>
     </>
   );
 };
